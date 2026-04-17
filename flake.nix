9fb2df4ce362292a357e282d8b5301e00faf7c73
Baptiste Esteban <baptiste.esteban@epita.fr>
2026-04-17 14:41:17 +0200

    Basic installation

--- flake.nix
+++ flake.nix
@@ -10,12 +10,34 @@
     };
   };
 
-  outputs = {nixpkgs, ...}: let
+  outputs = {self, nixpkgs, ...}: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
+    stdenv = pkgs.stdenv;
   in {
+    packages.${system}.default = stdenv.mkDerivation {
+      pname = "zagan";
+      version = "dev";
+
+      src = ./.;
+      nativeBuildInputs = with pkgs; [ cmake gnumake gcc ninja ];
+
+      configurePhase = ''
+        cmake --preset release --install-prefix=$out
+      '';
+      buildPhase = ''
+        cmake --build --preset release
+      '';
+      installPhase = ''
+        mkdir $out
+        cmake --install build/Release
+      '';
+
+    };
+
     devShell.${system} = pkgs.mkShell {
-      buildInputs = with pkgs; [cmake ninja gcc clang-tools gtest];
+      packages = with pkgs; [clang-tools gtest];
+      inputsFrom = with pkgs; [self.packages.${system}.default];
     };
 
     formatter.${system} = pkgs.alejandra;
