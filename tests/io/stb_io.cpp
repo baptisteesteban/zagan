769b2a337f7384c1d99719c22b098c8d7b7b3011
Baptiste Esteban <baptiste.esteban@epita.fr>
2026-06-05 11:44:08 +0200

    Update IO Tests

--- tests/io/stb_io.cpp
+++ tests/io/stb_io.cpp
@@ -8,17 +8,18 @@ TEST(IO, stb_uint8)
 {
   std::uint8_t values[] = {1, 3, 7, 3, 0, 6};
 
-  auto out = zagan::image2d<std::uint8_t>(3, 2);
-  for (int i = 0; i < 6; i++)
-    out(i % 3, i / 3) = values[i];
+  auto out = zagan::image2d<std::uint8_t>{{1, 3, 7}, {3, 0, 6}};
 
   zagan::io::imsave("tmp_uint8.png", out);
   zagan::image2d<std::uint8_t> in;
   zagan::io::imread("tmp_uint8.png", in);
   ASSERT_EQ(in.width(), out.width());
   ASSERT_EQ(in.height(), out.height());
-  for (int i = 0; i < 6; i++)
-    ASSERT_EQ(in(i % 3, i / 3), values[i]);
+  for (int y = 0; y < out.height(); y++)
+  {
+    for (int x = 0; x < out.width(); x++)
+      ASSERT_EQ(in(x, y), out(x, y));
+  }
 
   try
   {
